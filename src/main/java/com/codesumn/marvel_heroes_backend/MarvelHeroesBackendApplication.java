053cca9e5438c56166e618904f93a0395b6e936f
Thiago Rodrigues <thiagothogs@icloud.com>
2024-06-19 10:37:47 -0300

    Initial commit
    - Set up the basic structure for the Spring Boot project.
    - Added main application class `MarvelHeroesBackendApplication.java`.
    - Configured build tools with Gradle.
    - Included initial dependencies in `build.gradle`.
    - Added application properties file for basic configuration.
    - Created placeholder package structure for controllers, services, and repositories.
    - Included .gitignore file to exclude unnecessary files from the repository.
    
    This commit lays the foundation for the Marvel Heroes backend project.

---
+++ src/main/java/com/codesumn/marvel_heroes_backend/MarvelHeroesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codesumn.marvel_heroes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelHeroesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarvelHeroesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codesumn/marvel_heroes_backend/MarvelHeroesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesumn.marvel_heroes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelHeroesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
